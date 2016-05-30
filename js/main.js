43d800213a311eb8caf70f160cb2adc9d1604d8d
Christina Deemer <cahdeemer@gmail.com>
2016-05-30 18:03:57 -0400

    responsive homepage slider

--- js/main.js
+++ js/main.js
@@ -7,7 +7,40 @@ $(function(){
   $('.homepage-slider').slick({
     arrows: true,
     dots: false,
-    slidesToShow: 4
+    slidesToShow: 4,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        arrows: false
+        
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
   
   $('.grid').isotope({
