76521d92fe05cce4accd67d96b60031b3f006680
Christina Deemer <cahdeemer@gmail.com>
2016-06-16 09:48:37 -0400

    added favicons

--- header.php
+++ header.php
@@ -36,6 +36,24 @@
 		<link rel="stylesheet" href="<?php printThemePath(); ?>/style.css">
 		<link rel="stylesheet" href="<?php printThemePath(); ?>/css/main.css">
 		
+		
+		<!-- favicons and icons -->
+		<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png">
+	    <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png">
+	    <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png">
+	    <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png">
+	    <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png">
+	    <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png">
+	    <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png">
+	    <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png">
+	    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png">
+	    <link rel="icon" type="image/png" href="favicons/favicon-32x32.png" sizes="32x32">
+	    <link rel="icon" type="image/png" href="favicons/favicon-96x96.png" sizes="96x96">
+	    <link rel="icon" type="image/png" href="favicons/favicon-16x16.png" sizes="16x16">
+		
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
+		
 		<!--[if lt IE 9]>
 		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 		<![endif]-->
