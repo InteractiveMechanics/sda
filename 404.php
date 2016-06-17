8f082b0181dc1b5730fadf9dda4286b7c460ea18
Christina Deemer <cahdeemer@gmail.com>
2016-06-17 15:44:46 -0400

    styled 404 page

--- 404.php
+++ 404.php
@@ -1,24 +1,39 @@
 <?php get_header(); ?>
 
-	<main role="main">
+
+
+
+	<main role="main" class="error-page">
+		
+		
+		
+		
 		<!-- section -->
-		<section>
+		
+			
+			<div class="container">
+				<div class="row indented-container">
+					<div class="col-sm-9">
 
-			<!-- article -->
-			<article id="post-404">
-
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
-
-			</article>
-			<!-- /article -->
-
-		</section>
-		<!-- /section -->
+				<!-- article -->
+						<article id="post-404">
+			
+								<h1><?php _e( '404: Page not found', 'html5blank' ); ?></h1>
+								
+								<div class="error-page-message">
+									<h3>Oops!  We can't find the page you're looking for.</h3>
+								</div>
+								
+								<p>
+									<a href="<?php echo home_url(); ?>"><?php _e( "You might like to try going back to the home page.", 'sda_theme' ); ?></a>
+								</p>
+			
+						</article>
+						<!-- /article -->
+					</div>
+				</div>
+			</div>
 	</main>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
